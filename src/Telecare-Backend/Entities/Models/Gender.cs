4bb34dc924d23ec0768bb456337d9cb12e0ea2c9
sojib <mdsojibhosen444@gmail.com>
2023-09-24 11:28:54 +0600

    Designing Entities

--- src/Telecare-Backend/Entities/Models/Gender.cs
+++ src/Telecare-Backend/Entities/Models/Gender.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Entities.Models
+﻿namespace Entities.Models
 {
-    internal class Gender
+    public enum Gender
     {
+        Male,
+        Femail,
+        TransGender
     }
 }
