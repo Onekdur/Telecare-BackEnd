515d52ef4b14199eaafb80fd9855db8639b138ee
sojib <mdsojibhosen444@gmail.com>
2023-09-26 17:15:23 +0600

    Working to generate migration File

--- src/Telecare-Backend/Telecare.Application/Contract/IApplicationDbContext.cs
+++ src/Telecare-Backend/Telecare.Application/Contract/IApplicationDbContext.cs
@@ -1,7 +1,11 @@
-﻿namespace Telecare.Application.Contract
+﻿using Entities.Models.Member;
+using Microsoft.EntityFrameworkCore;
+
+namespace Telecare.Application.Contract
 {
     public interface IApplicationDbContext: IDisposable,IAsyncDisposable
     {
-
+        DbSet<Member> members { get; }
+        DbSet<TEntity> DbSet<TEntity>() where TEntity : class;        
     }
 }
---
+++ src/Telecare-Backend/Telecare.Persistance/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Models.Member;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Telecare.Application.Contract;
+
+namespace Telecare.Persistance.Contexts
+{
+    public class ApplicationDbContext : IdentityDbContext<Member>, IApplicationDbContext
+    {
+        public ApplicationDbContext()
+        {
+            
+        }
+
+        public DbSet<Member> members => Set<Member>();
+
+        public DbSet<TEntity> DbSet<TEntity>() where TEntity : class
+        {
+            return Set<TEntity>();
+        }
+    }
+}
