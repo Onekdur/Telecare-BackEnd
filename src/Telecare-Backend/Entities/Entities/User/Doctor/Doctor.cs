4f9436fa1deaccc5647313760622f23a4642550e
sojib <mdsojibhosen444@gmail.com>
2023-09-29 20:35:25 +0600

    commit

---
+++ src/Telecare-Backend/Entities/Entities/User/Doctor/Doctor.cs
@@ -0,0 +1,9 @@
+﻿namespace Telecare.Domain.Entities.User.Doctor
+{
+    public class Doctor
+    {
+        public Guid DoctorID { get; set; }
+        public string FullName { get; set; }
+        public string Experties { get; set; }
+    }
+}
